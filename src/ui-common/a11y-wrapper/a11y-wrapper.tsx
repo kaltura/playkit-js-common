369e738f3fcd12f8d868d72d88535d1efca22d3f
Serhii Marchenko <Serhii.marchenko@kaltura.com>
2022-10-10 14:10:52 +0300

    FEV-1439: update a11y wrapper

--- src/ui-common/a11y-wrapper/a11y-wrapper.tsx
+++ src/ui-common/a11y-wrapper/a11y-wrapper.tsx
@@ -1,16 +1,16 @@
-import {cloneElement, VNode} from 'preact';
+import { cloneElement, VNode } from 'preact';
 
-const {ENTER, SPACE} = KalturaPlayer.ui.utils.KeyMap;
+const { ENTER, SPACE } = KalturaPlayer.ui.utils.KeyMap;
 
 export type OnClickEvent = KeyboardEvent | MouseEvent;
-export type OnClick = (e: OnClickEvent, byKeyboard?: boolean) => void;
+export type OnClick = (e: OnClickEvent, byKeyboard: boolean) => void;
 
 interface A11yWrapperProps {
   children: VNode;
   onClick: OnClick;
 }
 
-export const A11yWrapper = ({children, onClick}: A11yWrapperProps) => {
+export const A11yWrapper = ({ children, onClick }: A11yWrapperProps) => {
   return cloneElement(children, {
     onKeyDown: (e: KeyboardEvent) => {
       if ([SPACE, ENTER].includes(e.keyCode)) {
@@ -19,8 +19,10 @@ export const A11yWrapper = ({children, onClick}: A11yWrapperProps) => {
       }
     },
     onClick: (e: MouseEvent) => {
+      // space/enter keyEvent is swallowed by NVDA (https://github.com/nvaccess/nvda/issues/7898)
+      const nvdaReaderEnabled = [e.offsetX, e.offsetY].every((offset) => offset === 0);
       e.stopPropagation();
-      onClick(e);
+      onClick(e, nvdaReaderEnabled);
     }
   });
 };
