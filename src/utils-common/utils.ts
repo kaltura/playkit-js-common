d5864bb458cdb145dfaa081d0d4ade6f1a6ce5e3
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2022-12-15 13:50:31 -0500

    add debounce method to utils

--- src/utils-common/utils.ts
+++ src/utils-common/utils.ts
@@ -32,3 +32,35 @@ function _generateAnonymousUserId() {
   const DefaultAnonymousPrefix = 'Guest';
   return `##${DefaultAnonymousPrefix}${HashSeparatorText}${generateNumberedId()}##`;
 }
+
+type Procedure = (...args: any[]) => void;
+export function debounce<F extends Procedure>(
+    func: F,
+    waitMilliseconds = 50,
+    options = {
+      isImmediate: false
+    }
+): F {
+  let timeoutId: any;
+
+  return function (this: any, ...args: any[]) {
+    const doLater = () => {
+      timeoutId = undefined;
+      if (!options.isImmediate) {
+        func.apply(this, args);
+      }
+    };
+
+    const shouldCallNow = options.isImmediate && timeoutId === undefined;
+
+    if (timeoutId !== undefined) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(doLater, waitMilliseconds);
+
+    if (shouldCallNow) {
+      func.apply(this, args);
+    }
+  } as any;
+}
