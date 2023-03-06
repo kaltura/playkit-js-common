45f58fc16b3c454aec889dbbb14f28474014a810
Serhii Marchenko <serhii.marchenko@kaltura.com>
2023-03-06 13:03:07 +0200

    chore: add share icon

--- src/icon/icons/index.ts
+++ src/icon/icons/index.ts
@@ -4,11 +4,13 @@ import { Print } from './print';
 import { Download } from './download';
 import { Related } from './related';
 import { Message } from './message';
+import { Share } from './share';
 
 export const Icons: Record<string, () => h.JSX.Element> = {
   close: Close,
   print: Print,
   download: Download,
   related: Related,
-  message: Message
+  message: Message,
+  share: Share
 };
