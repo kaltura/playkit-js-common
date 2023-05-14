0d189102036cbbffedd72bb87abd32c0f4aaf8c6
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2023-05-14 11:25:19 +0300

    fix(FEC-13130): #of/no results search should be announced

---
+++ src/hoc/sr-wrapper/index.tsx
@@ -0,0 +1,27 @@
+import { h, createContext, Component } from "preact";
+import * as styles from './sr-wrapper.scss';
+
+export const ScreenReaderContext = createContext((textToRead: string, delay?: number) => {});
+export class ScreenReaderProvider extends Component {
+  state = {
+    textToRead: ''
+  }
+
+  private _setTextToRead = (text: string, delay = 500) => {
+    setTimeout(() => {
+      this.setState({textToRead: text})
+    }, delay);
+  };
+
+  render() {
+    return (
+      <ScreenReaderContext.Provider value={this._setTextToRead}>
+        {this.props.children}
+        <div style={styles.srWrapper} aria-live={'polite'}>
+          <span id='sr-only' aria-label={this.state.textToRead}>
+            {this.state.textToRead}
+          </span>
+        </div>
+      </ScreenReaderContext.Provider> );
+  }
+}
