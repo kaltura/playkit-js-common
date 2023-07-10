28b6753afeeecc48d4f1f909ff151f1cabc0f8d4
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2023-07-10 14:42:09 +0300

    feat(FEC-13110): add icons (#27)
    - add the following icons: hotspot, chapter, arrow top-right, arrow bottom-left, answer on air.
    
    **related to PR:** https://github.com/kaltura/playkit-js-timeline/pull/11

---
+++ src/icon/icons/chapter.tsx
@@ -0,0 +1,7 @@
+import { h } from 'preact';
+
+export const Chapter = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M4 4C4 3.44772 4.44772 3 5 3H11C11.5523 3 12 3.44772 12 4V13C12 13.824 11.0592 14.2944 10.4 13.8L8.6 12.45C8.24444 12.1833 7.75556 12.1833 7.4 12.45L5.6 13.8C4.94076 14.2944 4 13.824 4 13V4Z" fill="white"/>
+  </svg>
+);
