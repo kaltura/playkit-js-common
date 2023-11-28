b33d893e497c6c1198756b0541cd3ca8177da2c4
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2023-11-28 15:45:21 +0200

    remove contrib services

--- src/ui-common/index.ts
+++ src/ui-common/index.ts
@@ -1,7 +1,2 @@
 // contrib-lib leftovers
-export { ContribServices } from './contrib-services';
-export { ToastSeverity } from './toast-manager';
-export { FloatingItem } from './floating-item';
-export { FloatingPositions, FloatingUIModes, FloatingItemProps } from './floating-item-data';
-export { BannerManager, VisibilityMode, BannerState } from './banner-manager';
 export { LinkifyString } from './linkify-string';
