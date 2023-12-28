f2deeb975aa3c68202851e03b8efc95e64da92db
Serhii Marchenko <serhii.marchenko@kaltura.com>
2023-12-28 15:06:55 +0200

    chore: fix type for children of OverlayPortal component

--- src/hoc/overlay-portal/index.tsx
+++ src/hoc/overlay-portal/index.tsx
@@ -1,15 +1,15 @@
-import { VNode,Component } from 'preact';
+import { ComponentChild, Component } from 'preact';
 
 const {
   redux: { connect },
-  reducers: {shell}
+  reducers: { shell }
 } = KalturaPlayer.ui;
 const { createPortal } = KalturaPlayer.ui;
 
 const PORTAL_CLASS = '.overlay-portal';
 
 export interface OverlayPortalProps {
-  children: VNode;
+  children: ComponentChild;
   targetId?: string;
   addPlayerClass?: () => void;
 }
@@ -23,8 +23,8 @@ const mapStateToProps = (state: Record<string, any>) => ({
 const mapDispatchToProps = (dispatch: any) => {
   return {
     addPlayerClass: () => dispatch(shell.actions.addPlayerClass(OVERLAY_ACTIVE_CLASS_NAME))
-  }
-}
+  };
+};
 
 @connect(mapStateToProps, mapDispatchToProps)
 export class OverlayPortal extends Component<OverlayPortalProps> {
@@ -36,7 +36,7 @@ export class OverlayPortal extends Component<OverlayPortalProps> {
       // use timeout 0 to make sure addPlayerClass happens after removePlayerClass
       setTimeout(() => {
         this.props.addPlayerClass!();
-      },0);
+      }, 0);
     }
   }
 
